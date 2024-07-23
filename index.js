73e65c856c7597c789b9a7094599b95b74b901b8
Kamran Siddique <72983784+ARK-09@users.noreply.github.com>
2024-07-22 20:25:49 -0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const summaryTrigers = document.querySelectorAll(".has-summary");
+  const overlay = document.querySelector(".overlay");
+  const overlayCloseBtn = document.querySelector(".btn-close");
+
+  summaryTrigers.forEach(function (summaryTriger) {
+    summaryTriger.addEventListener("click", showSummary);
+  });
+  overlay.addEventListener("click", hideSummary);
+  overlayCloseBtn.addEventListener("click", hideSummary);
+
+  function hideSummary() {
+    document.body.classList.remove("overlay-ative");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  }
+
+  function showSummary() {
+    document.body.classList.add("overlay-ative");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  }
+});
